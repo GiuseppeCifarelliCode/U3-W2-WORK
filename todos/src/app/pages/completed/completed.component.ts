8893721602b1e16bf17626f5a60aad49aede9b6d
Giuseppe Cifarelli <cifarelli1995@gmail.com>
2023-07-28 16:11:01 +0200

    Edit doesn't work

--- todos/src/app/pages/completed/completed.component.ts
+++ todos/src/app/pages/completed/completed.component.ts
@@ -10,19 +10,26 @@ import { TodosService } from 'src/app/todos.service';
 export class CompletedComponent {
 
   todos:Todo[] = []
+  emptyTodo:boolean = false
+  fetchCompleted:boolean = false
   private filterURL:string = "http://localhost:3000/todos?completed=true"
+
   constructor(private todoSvc:TodosService){}
 
   ngOnInit(){
     this.todoSvc.get(this.filterURL)
-    .then(todo =>
-      this.todos = todo)
+    .then(todo => {
+      this.todos = todo
+      this.fetchCompleted = !this.fetchCompleted
+        if(this.todos.length === 0) {this.emptyTodo = true}
+    })
+
   }
 
   delete(todo:Todo){
     this.todoSvc.delete(todo)
     this.todos = this.todos.filter(el => todo.title != el.title)
-
+    if(this.todos.length === 0) this.emptyTodo = true
 
   }
 }
--- todos/src/app/pages/todo/todo.component.ts
+++ todos/src/app/pages/todo/todo.component.ts
@@ -13,12 +13,17 @@ export class TodoComponent {
     newTodo:Todo = new Todoclass('', false)
     todos:Todo[] = []
     check:boolean[] = []
-    constructor(private todoSvc:TodosService){}
+    emptyTodo:boolean = false
+    fetchCompleted:boolean = false
+    constructor(private todoSvc:TodosService){
+    }
 
     ngOnInit(){
       this.todoSvc.get()
       .then(todo => {
         this.todos = todo
+        this.fetchCompleted = !this.fetchCompleted
+        if(this.todos.length === 0) {this.emptyTodo = true}
         todo.forEach((el,i) => this.check[i] = el.completed)
       })
     }
@@ -27,17 +32,18 @@ export class TodoComponent {
       this.todoSvc.create(this.newTodo)
       this.todos.push(this.newTodo)
       this.newTodo = new Todoclass('', false)
+      this.emptyTodo = false
     }
 
-    editTask(todo:Todo){
+    editTask(todo:Todo, i:number){
       this.newTodo = todo
+      this.todos[i].title = todo.title
+      this.todoSvc.edit(todo)
     }
 
     checkTask(todo:Todo,i:number){
       this.check[i] = todo.completed
       todo.completed = todo.completed === true ? false : true
       this.todoSvc.edit(todo)
-      .then( ()=> {
-      })
     }
 }
